5680b9f5608cc70d67eff0a563a934ddbb323a3a
Shrivathsan Sridhar <vathsansridhar@gmail.com>
2023-12-12 00:50:59 +0530

    Initial Commit

---
+++ setupEditor.js
@@ -0,0 +1,45 @@
+import { basicSetup } from "codemirror";
+import { json } from "@codemirror/lang-json";
+import { EditorState } from "@codemirror/state";
+import { indentWithTab } from "@codemirror/commands";
+import { EditorView, keymap } from "@codemirror/view";
+
+export default function setupEditors() {
+    const jsonRequestBody = document.querySelector('[data-json-request-body]');
+    const jsonResponseBody = document.querySelector('[data-json-response-body]');
+
+    const basicExtensions = [
+        basicSetup,
+        keymap.of([indentWithTab]),
+        json(),
+        EditorState.tabSize.of(2)
+    ];
+
+    const requestEditor = new EditorView({
+        state: EditorState.create({
+            doc: '{\n\t\n}',
+            extensions: basicExtensions
+        }),
+        parent: jsonRequestBody 
+    });
+    
+    const responseEditor = new EditorView({
+        state: EditorState.create({
+            doc: '{}',
+            extensions: [...basicExtensions, EditorView.editable.of(false)]
+        }),
+        parent: jsonResponseBody 
+    });
+
+    function updateResponseEditor(value) {
+        responseEditor.dispatch({
+            changes: {
+                from: 0,
+                to: responseEditor.state.doc.length,
+                insert: JSON.stringify(value, null, 2)
+            }
+        });
+    }
+
+    return { requestEditor, updateResponseEditor };
+}
